5ec27b9457e1958dd5e33420f6035a8c21ebeb4c
HellFireInfernoStorm <dinura2003@gmail.com>
2024-12-13 08:17:54 +0530

    lol

--- manual_control/__main__.py
+++ manual_control/__main__.py
@@ -1,16 +1,36 @@
 # main.py
 
 import pygame
+import threading
+import serial
+import os
 import config
 from src.camera import Camera
 from src.display import Display
 from src.comm import SerialComm
+
+serialThreadFlag = True
+
+def readSerial(comm: serial.Serial):
+    while serialThreadFlag:
+        if comm.in_waiting > 0:
+            line = comm.readline().decode('utf-8', errors='replace').strip()  # Read and strip extra spaces/newlines
+            if line:
+                if "=" in line:
+                    os.system("cls")
+                else:
+                    print(line)
 
 def main():
     # Initialize camera and display
     camera = Camera()
     display = Display()
     comm = SerialComm()
+
+    # Serial communication function
+    global serialThreadFlag
+    serialThread = threading.Thread(target=readSerial, name='serialThread', args=[comm], daemon=True)
+    serialThread.start()
 
     # Main application loop
     running = True
@@ -34,6 +54,8 @@ def main():
     # Clean up resources
     camera.release()
     display.close()
+    serialThreadFlag = False
+    serialThread.join()
 
 
 if __name__ == "__main__":
